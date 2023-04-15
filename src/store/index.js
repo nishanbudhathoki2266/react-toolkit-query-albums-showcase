c0b87949a830cd9834c0aacafd834cd44652befd
Nishan Budhathoki <nishanbudhathoki2266@gmail.com>
2023-04-15 14:16:25 +0545

    Implemented all the features for photos and finalised the project

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer } from "./slices/usersSlice";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
   },
   middleware: (getDefaultMiddleWare) => {
-    return getDefaultMiddleWare().concat(albumsApi.middleware);
+    return getDefaultMiddleWare()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware);
   },
 });
 
@@ -24,3 +28,9 @@ export {
   useAddAlbumMutation,
   useRemoveAlbumMutation,
 } from "./apis/albumsApi";
+
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} from "./apis/photosApi";
